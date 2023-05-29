8f869cef327cada55f2c447f4c3399b53c89ba54
Zihad550 <dev.jehadhossain@gmail.com>
2023-05-29 19:26:53 +0600

    feat: setup middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 const port = process.env.PORT || 9000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to hospital management server');
