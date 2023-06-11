21d8a6235cc8dd405585d82dc38a056227b81335
Zihad550 <dev.jehadhossain@gmail.com>
2023-06-11 08:27:29 +0600

    feat: update create user validation schema

--- src/app/modules/user/user.valiation.ts
+++ src/app/modules/user/user.valiation.ts
@@ -2,8 +2,10 @@ import { z } from 'zod'
 
 const createUserZodSchema = z.object({
   body: z.object({
-    role: z.string({ required_error: 'role is required' }),
-    password: z.string().optional(),
+    user: z.object({
+      role: z.string({ required_error: 'role is required' }),
+      password: z.string().optional(),
+    }),
   }),
 })
 
